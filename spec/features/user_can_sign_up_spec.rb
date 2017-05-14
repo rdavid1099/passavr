3f1d41ff0a18534581532fac1b143259e19ce7bf
Ryan Workman <rdavid1099@gmail.com>
2017-05-14 11:48:25 -0600

    Add test for user sign up and sad path

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -10,10 +10,25 @@ describe 'user clicks sign up' do
       fill_in 'user_password', with: 't3stP@ssword'
       fill_in 'user_password_confirmation', with: 't3stP@ssword'
       click_on 'Create Account'
-      byebug
 
-      expect(ActionMailer::Base.deliveries).to eq(1)
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
       expect(current_path).to eq(root_path)
+    end
+  end
+
+  context 'fills out improper information' do
+    it 'displays error if password is too short' do
+      visit root_path
+      click_on 'Login/ Sign Up'
+      click_on 'Create New Account'
+      fill_in 'user_email', with: 'test@test.com'
+      fill_in 'user_password', with: 'test'
+      fill_in 'user_password_confirmation', with: 'tst'
+      click_on 'Create Account'
+
+      expect(page).to have_content("Password confirmation doesn't match Password")
+      expect(page).to have_content('Password is too short (minimum is 12 characters)')
+      expect(page).to have_content('Password must include at least one lowercase letter, one uppercase letter, and one digit')
     end
   end
 end
