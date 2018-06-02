0c3842f21a6b647586ed04506b93f8bec3c12ade
Ryan Workman <rdavid1099@gmail.com>
2018-06-02 12:04:33 -0600

    Add shoulda matchers and base user tests

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_05_31_235419) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
